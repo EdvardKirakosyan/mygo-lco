8de56039bcbd00bf4c14dc1abf424486a43c6080
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2025-01-25 17:45:16 +0400

    c

--- 12ifelse/main.go
+++ 12ifelse/main.go
@@ -4,4 +4,31 @@ import "fmt"
 
 func main() {
 	fmt.Println("If Else in GO")
+
+	loginCount := 23
+
+	var result string
+
+	if loginCount < 10 {
+		result = "Regular user"
+	} else if loginCount > 10 {
+		result = "watch out"
+	} else {
+		result = "exactly 10"
+	}
+
+	fmt.Println(result)
+
+	if 9%2 == 0 {
+		fmt.Println("Num is even")
+	} else {
+		fmt.Println("Num is odd")
+	}
+
+	if num := 3; num < 10 {
+		fmt.Println("Num is less than 10")
+	} else {
+		fmt.Println("Num is not less than 10")
+	}
+
 }
